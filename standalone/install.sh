1cda7e9692173e5f367c0a82b4c0b7808c1aae19
Riven Skaye <riven@tae.moe>
2025-04-24 14:36:44 +0200

    Don't run if useless, split the crons for compat

--- standalone/install.sh
+++ standalone/install.sh
@@ -10,8 +10,14 @@ cp ../porkcron.py /etc/porkcron/porkcron.py
 chmod +x /etc/porkcron/porkcron.py
 
 # Add a cron entry for it to run on reboot and weekly
-echo '@weekly /usr/local/bin/porkcron' > /etc/cron.d/porkcron
-echo '@reboot /usr/local/bin/porkcron' >> /etc/cron.d/porkcron
+echo '0 0 * * 0 /usr/local/bin/porkcron' > /etc/cron.d/porkcron.weekly
+echo '@reboot /usr/local/bin/porkcron' >> /etc/cron.d/porkcron.reboot
 
-# Run once
-/usr/local/bin/porkcron
+# Run once if the .env file exists
+if [ -e ../.env ]
+then
+  cp ../.env /etc/porkcron/.env
+  /usr/local/bin/porkcron
+else
+  echo "Please fill out the required values for $(dirname "$PWD")/.env.example and copy or move it to /etc/porkcron/.env before running porkcron for the first time."
+fi

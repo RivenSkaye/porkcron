a3caf72aabedf15b01fcf374b860b29f74f8cb1e
Riven Skaye <riven@tae.moe>
2025-04-28 13:35:03 +0200

    Found another working solution!

--- standalone/porkcron.sh
+++ standalone/porkcron.sh
@@ -6,16 +6,7 @@ cd /etc/porkcron
 # Read the .env file if it exists
 if [ -e .env ]
 then
-  while IFS== read -r key value; do
-    case $key in
-      ''|\ \#*|\#*) continue ;;
-    esac
-    case $value in
-      \ \#*|\#*) continue ;;
-    esac
-    printf -v "$key" %s "$value"
-    export "$key"
-  done <.env
+  export $(grep -v '^\s*#' .env | xargs | sed 's/, /,/g')
 else
   echo "Could not find $PWD/.env to fetch the latest SSL certs!" 1>&2
   echo "Trying anyway, it should work if the correct environment variables are set elsewhere." 1>&2
